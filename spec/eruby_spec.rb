e1d18a1f88f8ea667015b746495ebc1c5d374ec8
Andrew Radev <andrey.radev@gmail.com>
2012-08-08 17:05:01 +0300

    Spec overrides

--- spec/eruby_spec.rb
+++ spec/eruby_spec.rb
@@ -82,4 +82,75 @@ describe "eruby definitions" do
 
     VIM.switch; assert_file_contents '<%# something %>'
   end
+
+  describe "(overrides)" do
+    specify "true/false overrides if-clauses" do
+      set_file_contents <<-EOF
+        <% if false and (predicate?) %>
+          <%= 'Hello, World!' %>
+        <% end %>
+      EOF
+
+      VIM.search('false').switch
+      assert_file_contents <<-EOF
+        <% if true and (predicate?) %>
+          <%= 'Hello, World!' %>
+        <% end %>
+      EOF
+
+      VIM.normal('u').search('if').switch
+      assert_file_contents <<-EOF
+        <% if predicate? %>
+          <%= 'Hello, World!' %>
+        <% end %>
+      EOF
+    end
+
+    specify "true/false overrides hash style" do
+      set_file_contents <<-EOF
+        <% foo = { :one => true } %>
+      EOF
+
+      VIM.search('true').switch
+      assert_file_contents <<-EOF
+        <% foo = { :one => false } %>
+      EOF
+
+      VIM.normal('u').search('one').switch
+      assert_file_contents <<-EOF
+        <% foo = { one: true } %>
+      EOF
+    end
+
+    specify "true/false overrides tag type" do
+      set_file_contents '<% true %>'
+
+      VIM.search('true').switch
+      assert_file_contents '<% false %>'
+
+      VIM.normal('u').search('<%').switch
+      assert_file_contents '<%# true %>'
+    end
+
+    specify "hash style overrides tag type" do
+      set_file_contents '<% {:one => two} %>'
+
+      VIM.search('one').switch
+      assert_file_contents '<% {one: two} %>'
+
+      VIM.normal('u').search('<%').switch
+      assert_file_contents '<%# {:one => two} %>'
+    end
+
+    specify "if-clauses override tag type" do
+      set_file_contents <<-EOF
+        <% if predicate? %>
+      EOF
+
+      VIM.switch
+      assert_file_contents <<-EOF
+        <% if true or (predicate?) %>
+      EOF
+    end
+  end
 end
